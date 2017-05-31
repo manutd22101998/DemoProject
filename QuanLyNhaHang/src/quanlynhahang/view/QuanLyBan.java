0bff73e77713028d8de1d1199024e5cf06d2a238
manutd22101998 <tienvmd00279@fpt.edu.vn>
2017-05-31 13:15:29 +0700

    giao dien quan ly admin

---
+++ QuanLyNhaHang/src/quanlynhahang/view/QuanLyBan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlynhahang.view;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Anh Tiến ơi.Có Trộm!
+ */
+public class QuanLyBan extends JPanel {
+
+    public QuanLyBan() {
+        setBackground(new Color(48, 193, 204));
+        setBounds(400, 110, 1220, 560);
+
+//        JLabel tittle = new JLabel();
+//        tittle.setText("QUẢN LÝ ADMIN");
+//        tittle.setBounds(490, 0, 280, 70);
+//        tittle.setFont(new Font("Serif", Font.PLAIN, 34));
+
+//        add(tittle);
+        setVisible(true);
+        setLayout(null);
+    }
+
+}
